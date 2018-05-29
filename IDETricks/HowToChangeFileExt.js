bb92acfab3c478161145eebd9ddb2eae8975e12a
MariannaMilovanova <marianna.milovanova@binary-studio.com>
2018-05-29 11:10:13 +0300

    change file extension manually

--- IDETricks/HowToChangeFileExt.js
+++ IDETricks/HowToChangeFileExt.js
@@ -1,3 +1,4 @@
+/*
 Under Settings -> Editor -> File Types you can define file type patterns.
 
-Just scroll down under fille current ext and delete this file
+Just scroll down under fille current ext and delete this file*/
