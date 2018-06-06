b79c43ab589684d0c116b375d06cbe1abda0ce91
MariannaMilovanova <mmilovanova@kse.org.ua>
2018-06-06 21:48:36 +0300

    get

---
+++ FullStack/PrepareToDeploy.js
@@ -0,0 +1,25 @@
+//port should be dynamic
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
+//add to package.json node and npm versions
+/*
+"engines": {
+  "node": "8.9.1",
+    "npm": "5.5.1"
+},
+*/
+
+//specifies start script
+/*
+"scripts": {
+  "start": "node index.js"
+},*/
+
+//add .gitignore file
+
+//install HEROKU CLI
+
+
+//in T: heroku login
+//in T: heroku create
