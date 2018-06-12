6e3dd41585f8b2a7fe95b9a19cf5558ec4166b70
MariannaMilovanova <mmilovanova@kse.org.ua>
2018-06-12 14:29:07 +0300

    async await construction

---
+++ PureJS/AsyncAwait.js
@@ -0,0 +1,19 @@
+function fetchAlbumsES2015() {
+  fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(res => {
+      const json = res.json();
+      console.log('1', json);
+      return json;
+    })
+    .then(json => console.log('2', json))
+}
+fetchAlbumsES2015();
+
+//with async await construction
+
+async function fetchAlbumsES2016() {
+  const res = await fetch('https://rallycoding.herokuapp.com/api/music_albums');
+  const json = await res.json();
+  console.log('3', json);
+}
+fetchAlbumsES2016();
