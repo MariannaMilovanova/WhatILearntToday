89e8f8a64ecbac834c133d331b5541a0183b49ae
Marianna Milovanova <mmilovanova@kse.org.ua>
2019-01-08 16:21:23 +0200

    SourceCodeNavigation

---
+++ Debug/SourceCodeNavigation.js
@@ -0,0 +1,11 @@
+//Go to a line number at a specific column
+
+/*
+GOTO a line number with a specified column using the format :linenumber:columnnumber in the CMD+O dialogue from the Sources panel editor.
+
+    Hit CMD + O from the Sources Panel with an open file
+Enter :5:9
+Notice you are taken to line 5, column 9!*/
+
+const linkWithAGif = 'https://developers.google.com/web/updates/2015/05/go-to-a-line-number-at-a-specific-column';
+export default linkWithAGif
